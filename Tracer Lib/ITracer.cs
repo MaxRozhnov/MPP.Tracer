ab9b299a196af819c2b61c4c3dfd5168ed666e07
MaxRozhnov <skymax1998@gmail.com>
2018-10-30 19:23:26 +0300

    Code refactoring

--- Tracer Lib/ITracer.cs
+++ Tracer Lib/ITracer.cs
@@ -2,11 +2,8 @@ namespace Tracer_Lib
 {
     public interface ITracer
     {
-        // вызывается в начале замеряемого метода
         void StartTrace();
-        // вызывается в конце замеряемого метода
         void StopTrace();
-        // получить результаты измерений
         TraceResult GetTraceResult();
     }
 }
